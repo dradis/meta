d66360f1fa947607ea313f2625c57b5ac9a29833
Daniel Martin <etd(at)nomejortu.com>
2010-12-01 20:15:35 +0000

    Add two new helper scripts to:
    * Reset the environment
    * Start the server
    
    At the end of the day we want to make life easy for users, in an ideal world, 
    they should be able to run Dradis without knowing the first thing about Rails!
    
    TODO: convert these into a cross-platform Thor script.

---
+++ reset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Dradis Framework environment reset script 
+#
+# License:
+#   This file may be used under the terms of the GNU General Public
+# License version 2.0 as published by the Free Software Foundation
+# and appearing in the file LICENSE.txt included in the packaging of
+# this file.
+#
+# Copyright: Daniel Martin Gomez <etd[-at-]nomejortu.com>
+
+# sorry folks, nothing smart going on here
+RAILS_ENV=production
+cd server
+rake dradis:reset
