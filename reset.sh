cdabf53866f227344c167ee02f020712c80068e4
Daniel Martin <etd(at)nomejortu.com>
2010-12-03 14:41:35 +0000

    It is a good idea that the reset script checks the status of Bundler to offer the opportunity of installing missing dependencies.

--- reset.sh
+++ reset.sh
@@ -10,7 +10,30 @@
 #
 # Copyright: Daniel Martin Gomez <etd[-at-]nomejortu.com>
 
-# sorry folks, nothing smart going on here
 RAILS_ENV=production
 cd server
-rake dradis:reset
+
+bundle check
+
+if [ $? -eq 1 ] 
+then
+
+  echo -n "Some Ruby gems are missing, do you want to install them now? [y] "
+  read INSTALL
+
+  if [ $INSTALL = "y" ]
+  then
+    echo
+    echo -e "Ok then, I am going to run \033[1mbundle install\033[0m for you, then you should run this script again."
+    echo
+
+    bundle install
+  else
+    echo
+    echo -e 'You can install the gems manually by going to the ./server/ folder and running: \033[1mbundle install\033[0m'
+    echo
+  fi
+
+else
+  rake dradis:reset
+fi
