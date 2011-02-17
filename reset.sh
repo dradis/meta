71b0844bd8d5549e8d0fbedfeb0e4323188ce40f
Daniel Martin <etd(at)nomejortu.com>
2011-02-17 17:05:29 +0000

    * Export the RAILS_ENV variable so it is inherited in subshells * Remove the copyright notice. In project Mayhem we have no names.

--- reset.sh
+++ reset.sh
@@ -8,9 +8,8 @@
 # and appearing in the file LICENSE.txt included in the packaging of
 # this file.
 #
-# Copyright: Daniel Martin Gomez <etd[-at-]nomejortu.com>
 
-RAILS_ENV=production
+export RAILS_ENV=production
 cd server
 
 bundle check
